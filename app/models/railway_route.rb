0c30bc589408ecf6d767953edf81b0341ce47234
Tomoya.ikarashi <tomoya.ikarashi@luxiar.com>
2024-08-14 23:45:53 +0900

    リレーション追加

--- app/models/railway_route.rb
+++ app/models/railway_route.rb
@@ -1,4 +1,5 @@
 # 路線テーブル
 class RailwayRoute < ApplicationRecord
   belongs_to :railway_company
+  has_many :railway_route_stations
 end
--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,4 +2,5 @@
 class RailwayStation < ApplicationRecord
   belongs_to :prefecture
   belongs_to :city
+  has_many :railway_route_stations
 end
